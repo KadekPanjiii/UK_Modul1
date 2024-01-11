f8a61b8a48c131e3878537b1dbe954ed4026c029
KadekPanjiii <dekpanji23@gmail.com>
2024-01-11 15:30:05 +0800

    first commit

--- components/footer.php
+++ components/footer.php
@@ -17,23 +17,6 @@
 
         </div>
     </div>
-    <!-- Libs JS -->
-    <script
-      src="assets/dist/libs/apexcharts/dist/apexcharts.min.js?1684106062"
-      defer
-    ></script>
-    <script
-      src="assets/dist/libs/jsvectormap/dist/js/jsvectormap.min.js?1684106062"
-      defer
-    ></script>
-    <script
-      src="assets/dist/libs/jsvectormap/dist/maps/world.js?1684106062"
-      defer
-    ></script>
-    <script
-      src="assets/dist/libs/jsvectormap/dist/maps/world-merc.js?1684106062"
-      defer
-    ></script>
     <!-- Tabler Core -->
     <script src="assets/dist/js/tabler.min.js?1684106062" defer></script>
     <script src="assets/dist/js/demo.min.js?1684106062" defer></script>
